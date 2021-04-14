52ece5886821c1067007a822c5433cb7472d8ba8
misurac <72106662+misurac@users.noreply.github.com>
2021-04-14 12:29:57 -0500

    Add files via upload

---
+++ pong/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player = new System.Windows.Forms.PictureBox();
+            this.cpu = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Yellow;
+            this.player.Location = new System.Drawing.Point(12, 186);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(27, 127);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // cpu
+            // 
+            this.cpu.BackColor = System.Drawing.Color.Fuchsia;
+            this.cpu.Location = new System.Drawing.Point(883, 186);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(27, 127);
+            this.cpu.TabIndex = 1;
+            this.cpu.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ball.Location = new System.Drawing.Point(434, 239);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(27, 26);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(922, 557);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Pong Game Misurac";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox cpu;
+        private System.Windows.Forms.PictureBox ball;
+    }
+}
+
